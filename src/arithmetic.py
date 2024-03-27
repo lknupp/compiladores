4ab2c77993303cd492007ec912c106a9f3ad14ea
Leonam Knupp <leonamknupp@hotmail.com>
2024-03-27 16:54:35 -0300

    fix: convert methods to static and fix behavior of divide method

--- src/arithmetic.py
+++ src/arithmetic.py
@@ -1,62 +1,67 @@
+from abc import ABC, abstractmethod
 from .stack import Stack
 
 
-class Arithmetic:
+class Arithmetic(ABC):
     """
     This class contains methods for arithmetic operations
     """
-
-    def add(self, stack: Stack) -> None:
+    @abstractmethod
+    def add(stack: Stack) -> None:
         """
         This method pops two values from the stack and pushes their sum back to the stack
         :param stack: Stack object
         :return: None
         """
         try:
-            value = stack.pop() + stack.pop()
-            stack.push(value)
+            result: int = stack.pop() + stack.pop()
+            stack.push(result)
         except IndexError:
             raise IndexError("Stack is empty")
 
-    def subtract(self, stack: Stack):
+    @abstractmethod
+    def subtract(stack: Stack):
         """
         This method pops two values from the stack and pushes their difference back to the stack
         :param stack: Stack object
         :return: None
         """
         try:
-            value = stack.pop() - stack.pop()
-            stack.push(value)
+            result: int = stack.pop() - stack.pop()
+            stack.push(result)
         except IndexError:
             raise IndexError("Stack is empty")
 
-    def multiply(self, stack: Stack):
+    @abstractmethod
+    def multiply(stack: Stack):
         """
         This method pops two values from the stack and pushes their product back to the stack
         :param stack: Stack object
         :return: None
         """
         try:
-            value = stack.pop() * stack.pop()
-            stack.push(value)
+            result: int = stack.pop() * stack.pop()
+            stack.push(result)
         except IndexError:
             raise IndexError("Stack is empty")
 
-    def divide(self, stack: Stack):
+    @abstractmethod
+    def divide(stack: Stack):
         """
         This method pops two values from the stack and pushes their quotient back to the stack
         :param stack: Stack object
         :return: None
         """
         try:
-            value = stack.pop() / stack.pop()
-            stack.push(value)
+            result: int = stack.pop() // stack.pop()
+            stack.push(result)
         except IndexError:
             raise IndexError("Stack is empty")
         except ZeroDivisionError:
             raise ZeroDivisionError("Cannot divide by zero")
 
-    def modulo(self, stack: Stack):
+    @abstractmethod
+    def modulo(stack: Stack):
         """
         This method pops two values from the stack and pushes their modulo back to the stack
         :param stack: Stack object
@@ -64,8 +69,8 @@ class Arithmetic:
 
         """
         try:
-            value = stack.pop() % stack.pop()
-            stack.push(value)
+            result: int = stack.pop() % stack.pop()
+            stack.push(result)
         except IndexError:
             raise IndexError("Stack is empty")
         except ZeroDivisionError:
